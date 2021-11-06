ce2296701513824103afff7bbf5242647c7ed182
Shiwayari <shiwayari@gmail.com>
2021-11-07 00:32:05 +0100

    Add Baal character (lv81 scaling)

--- include/Character.hh
+++ include/Character.hh
@@ -56,6 +56,49 @@ private:
 		{ { DmgTalent::Burst, 1 }, Hit{ DmgTalent::Burst, DmgElement::Cryo, 202.0 * 19.0 + 303.0 } }
 	};
 };
+
+// -------------------------------------------------- Baal --------------------------------------------------
+
+class Baal : public Character {
+public:
+	unsigned int energy_consumed = 80 + 80 + 60;
+
+	Baal() :
+			Character{
+				{ .base_hp = 12090,
+				  .base_atk = 316,
+				  .base_def = 739,
+				  .crit_rate = 5.0,
+				  .crit_dmg = 50.0,
+				  .energy_recharge = 132.0 }
+			} {
+		DEBUG("___Baal___\n"
+			  << status);
+	}
+
+	Hit get_hit(DmgTalent, unsigned int hit_num) const override;
+	void apply_effects(Status&) const override;
+
+private:
+	inline static const Abilities ABILITIES{
+		{ { DmgTalent::Normal, 1 }, Hit{ DmgTalent::Normal, DmgElement::Phys, 57.6 } },
+		{ { DmgTalent::Normal, 2 }, Hit{ DmgTalent::Normal, DmgElement::Phys, 57.8 } },
+		{ { DmgTalent::Normal, 3 }, Hit{ DmgTalent::Normal, DmgElement::Phys, 72.5 } },
+		{ { DmgTalent::Normal, 4 }, Hit{ DmgTalent::Normal, DmgElement::Phys, 42.1 + 42.1 } },
+		{ { DmgTalent::Normal, 5 }, Hit{ DmgTalent::Normal, DmgElement::Phys, 95.1 } },
+		{ { DmgTalent::Charged, 1 }, Hit{ DmgTalent::Charged, DmgElement::Phys, 144.8 } },
+		{ { DmgTalent::Skill, 1 }, Hit{ DmgTalent::Skill, DmgElement::Electro, 187.5 } },		 // initial hit
+		{ { DmgTalent::Skill, 2 }, Hit{ DmgTalent::Skill, DmgElement::Electro, 67.2 } },		 // coord. attack
+		{ { DmgTalent::Burst, 1 }, Hit{ DmgTalent::Burst, DmgElement::Electro, 681.0 } },		 // initial hit
+		{ { DmgTalent::Burst, 2 }, Hit{ DmgTalent::Burst, DmgElement::Electro, 75.2 } },		 // N1
+		{ { DmgTalent::Burst, 3 }, Hit{ DmgTalent::Burst, DmgElement::Electro, 73.9 } },		 // N2
+		{ { DmgTalent::Burst, 4 }, Hit{ DmgTalent::Burst, DmgElement::Electro, 90.5 } },		 // N3
+		{ { DmgTalent::Burst, 5 }, Hit{ DmgTalent::Burst, DmgElement::Electro, 51.9 + 52.1 } },  // N4
+		{ { DmgTalent::Burst, 6 }, Hit{ DmgTalent::Burst, DmgElement::Electro, 124.4 } },		 // N5
+		{ { DmgTalent::Burst, 7 }, Hit{ DmgTalent::Burst, DmgElement::Electro, 103.6 + 125.1 } } // charged
+	};
+};
+
 }
 
 #endif
--- src/Character.cc
+++ src/Character.cc
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include <Character.hh>
 
 namespace GenshinCalc {
@@ -21,6 +23,22 @@ void Ayaka::apply_effects(Status& stats) const {
 	if (KantenSenmyouBlessing) stats.cryo_bonus += 18.0;
 }
 
+// -------------------------------------------------- Baal --------------------------------------------------
 
+Hit Baal::get_hit(DmgTalent talent, unsigned int hit_num) const {
+	auto hit = ABILITIES.at({ talent, hit_num });
+	if (hit.talent == DmgTalent::Burst) {
+		const float resolve = std::min(60.0, static_cast<float>(energy_consumed) * 0.19);
+		const float resolve_bonus = (hit_num == 1 ? 6.61 : 1.23) * resolve;
+		hit.scaling_perc += resolve_bonus;
+		if (hit_num == 5 || hit_num == 7) hit.scaling_perc += resolve_bonus; // these have two hits
+	}
+	return hit;
+}
+
+void Baal::apply_effects(Status& stats) const {
+	stats.electro_bonus += 0.4 * (stats.energy_recharge - 100.0);
+	stats.burst_bonus += 0.29 * 90;
+}
 
 }
