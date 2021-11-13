662495d585d946a83e48b6ecbaa1d048f435bab9
Shiwayari <shiwayari@gmail.com>
2021-11-13 19:54:26 +0100

    Add Whiteblind & Skyward Pride weapons

--- include/Weapon.hh
+++ include/Weapon.hh
@@ -20,10 +20,10 @@ class BlackSword : public Weapon {
 public:
 	BlackSword() :
 			Weapon{
-				{ .base_atk = 510,
-				  .crit_rate = 27.6,
-				  .normal_bonus = 35.0,
-				  .charged_bonus = 35.0 }
+				{ .base_atk = 510.0f,
+				  .crit_rate = 27.6f,
+				  .normal_bonus = 35.0f,
+				  .charged_bonus = 35.0f }
 			} {
 		DEBUG("___Black Sword___\n"
 			  << status);
@@ -38,15 +38,46 @@ class TheCatch : public Weapon {
 public:
 	TheCatch() :
 			Weapon{
-				{ .base_atk = 510,
-				  .crit_rate = 12.0,
-				  .energy_recharge = 45.9,
-				  .burst_bonus = 32.0 }
+				{ .base_atk = 510.0f,
+				  .crit_rate = 12.0f,
+				  .energy_recharge = 45.9f,
+				  .burst_bonus = 32.0f }
 			} {}
 
 	void apply_effects(Status&) const override {}
 };
 
+// -------------------------------------------------- Whiteblind --------------------------------------------------
+
+class Whiteblind : public Weapon {
+public:
+	Whiteblind(unsigned int refine = 1) :
+			Weapon{
+				Status{ .base_atk = 510.0f,
+				  .atk_perc = 18.0f + static_cast<float>(refine) * 6.0f,
+				  .def_perc = 51.7f + 18.0f + static_cast<float>(refine) * 6.0f }
+			} {}
+
+	void apply_effects(Status&) const override {}
+};
+
+// -------------------------------------------------- Skyward Pride --------------------------------------------------
+
+class SkywardPride : public Weapon {
+public:
+	SkywardPride(unsigned int refine = 1) :
+			Weapon{
+				{ .base_atk = 674.0f,
+				  .energy_recharge = 36.8f,
+				  .normal_bonus = 6.0f + static_cast<float>(refine) * 2.0f,
+				  .charged_bonus = 6.0f + static_cast<float>(refine) * 2.0f,
+				  .plunge_bonus = 6.0f + static_cast<float>(refine) * 2.0f,
+				  .skill_bonus = 6.0f + static_cast<float>(refine) * 2.0f,
+				  .burst_bonus = 6.0f + static_cast<float>(refine) * 2.0f }
+			} {}
+
+	void apply_effects(Status&) const override {}
+};
 }
 
 #endif
